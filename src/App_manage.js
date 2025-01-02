4b90202d092243b4a5da63deb876a36ae7f912d5
Muhammad-Sohail-Khan-Niazi <mskniazi01@gmail.cmm>
2025-01-02 11:49:40 +0500

    manage component

---
+++ src/App_manage.js
@@ -0,0 +1,9 @@
+import react from "react";
+//using components base App 
+import Header   from './Header.jsx';
+
+function manage(){
+<>
+<headfile></headfile>
+</>
+}
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+// import react from "react";
+import './App.css';
+
+function headfile (){
+  return (
+
+    <>
+    <div className="App"> 
+<h2> 2nd day of 2025
+</h2>
+    </div> 
+    </>
+  );
+}
